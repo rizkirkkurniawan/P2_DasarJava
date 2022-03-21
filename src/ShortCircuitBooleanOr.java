db20d42fe79e56a30bef749e29173edd0af11008
PC 05 <rizkirkkurniawan>
2022-03-21 20:16:24 +0700

    Menggunakan Boolean Or Short Circuit

---
+++ src/ShortCircuitBooleanOr.java
@@ -0,0 +1,10 @@
+public class ShortCircuitBooleanOr {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+        if ((a>2)||(b++< 10)){
+            b+=2;
+
+        }
+        System.out.println(b);
+
+    }}
