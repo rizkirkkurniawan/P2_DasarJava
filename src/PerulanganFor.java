8b0a11fea9bc8d7f488492bda9807c33cc907c5b
PC 05 <rizkirkkurniawan>
2022-03-21 20:35:09 +0700

    Menggunakan perulangan for

---
+++ src/PerulanganFor.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PerulanganFor {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("maukkan jumlah perulangan");
+        int jml=input.nextInt();
+        for (int i=0;i<jml;i++)
+            System.out.println("pemoograman berrorientasi objek dengan java!");
+    }
+}
