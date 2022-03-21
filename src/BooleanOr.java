caac7bbe6213ae5b281479169edf05fc98bf7b3e
PC 05 <rizkirkkurniawan>
2022-03-21 20:11:54 +0700

    Menggunakan Boolean Or

---
+++ src/BooleanOr.java
@@ -0,0 +1,11 @@
+public class BooleanOr {
+    public static void main(String[] args) {
+        int a = 5, b = 7;
+        if ((a > 2) | (b++ < 10)) {
+            b += 2;
+
+        }
+        System.out.println(b);
+
+    }
+}
