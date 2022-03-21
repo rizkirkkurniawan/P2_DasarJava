91a4f36b4d8058f28c55d7b652bdeb150207f4a7
PC 05 <rizkirkkurniawan>
2022-03-21 20:41:04 +0700

    Menggunakan perulangan for each

---
+++ src/PerulanganWhile.java
@@ -0,0 +1,20 @@
+public class PerulanganWhile {
+    public static void main(String[] args) {
+        //while loop
+        int x=0;
+        System.out.println("example of while loop--");
+        while (x<=10){
+            System.out.printf("%d",x);
+            x++;
+
+        }
+        System.out.println("\n");
+        //do-while loop
+        int y=0;
+        do {
+            y=y+10;
+            System.out.println("i am inside do blok---"+y);
+        }
+        while (y<=50);
+    }
+}
